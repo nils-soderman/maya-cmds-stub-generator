1813d416c085725687a7e751181bc55deb8f792d
Nils <contact@nilssoderman.com>
2025-09-15 21:22:39 +0200

    removed some dev stuff

--- cmds-stub-generator/documentation/__init__.py
+++ cmds-stub-generator/documentation/__init__.py
@@ -1,9 +1,1 @@
-import sys
-
-try:
-    import bs4
-except ModuleNotFoundError:
-    import subprocess # TODO: remote, Only for dev purposes
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "beautifulsoup4"])
-
 from . import index, command
