0d6f54c7f6ba18341512d832382184f0c3079f3a
Nils <contact@nilssoderman.com>
2025-09-15 21:22:22 +0200

    Added cli option `--undocumented`

--- cmds-stub-generator/__main__.py
+++ cmds-stub-generator/__main__.py
@@ -1,18 +1,29 @@
 import argparse
 import os
 
-from . import generator
+from . import generator, GeneratorFlag
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Generate type stubs for `maya.cmds` module.")
+    parser = argparse.ArgumentParser(description="Generate stubs for the `maya.cmds` module. This module must run in the mayapy interpreter.")
+
     parser.add_argument("output", type=str, help="Output file path for the generated stubs.")
+    parser.add_argument(
+        "--undocumented",
+        action="store_true",
+        help="Include internal functions not publicly documented"
+    )
+
     args = parser.parse_args()
 
     output_path = os.path.abspath(args.output)
-    generator.generate_stubs(output_path)
+
+    flags = GeneratorFlag.NONE
+    if args.undocumented:
+        flags |= GeneratorFlag.INCLUDE_UNDOCUMENTED_FUNCTIONS
+
+    generator.generate_stubs(output_path, flags=flags)
+
 
 if __name__ == "__main__":
     main()
-
-# "C:/Program Files/Autodesk/Maya2026/bin/mayapy.exe" -m cmds-stub-generator "generated-stubs/2026/cmds.pyi"
