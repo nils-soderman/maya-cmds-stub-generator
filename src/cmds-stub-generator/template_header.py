b3d97195ded61ab0d8fc940fcd0abbd2156743a3
Nils <contact@nilssoderman.com>
2025-09-17 21:31:37 +0200

    Updated header

--- src/cmds-stub-generator/template_header.py
+++ src/cmds-stub-generator/template_header.py
@@ -2,8 +2,9 @@
 maya.cmds stub file generated for Maya {VERSION} using:
 https://github.com/nils-soderman/maya-cmds-stub-generator
 """
-
 from __future__ import annotations
+
+from warnings import deprecated
 from typing import (
     TypeVar,
     TypeAlias,
@@ -14,14 +15,6 @@ from typing import (
     Any,
     overload
 )
-
-try:
-    from warnings import deprecated  # type: ignore
-except ImportError:
-    def deprecated(msg: str):
-        def decorator(func: Callable) -> Callable:
-            return func
-        return decorator
 
 T = TypeVar("T")
 multiuse: TypeAlias = Union[Sequence[T], T]
